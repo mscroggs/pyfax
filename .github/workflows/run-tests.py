11e2731ce73da8565ae437cf2bde440b4797033b
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-06-10 09:55:18 +0100

    update workflows

--- .github/workflows/run-tests.py
+++ .github/workflows/run-tests.py
@@ -12,12 +12,15 @@ jobs:
   build-pages:
     name: Install and test
     runs-on: ubuntu-latest
+    strategy:
+      matrix:
+        python-version: ["3.7", "3.8", "3.9", "3.10"]
     steps:
-      - uses: actions/checkout@v3
       - name: Set up Python
         uses: actions/setup-python@v3
         with:
-          python-version: "3.10"
+          python-version: ${{ matrix.python-version }}
+      - uses: actions/checkout@v3
       - run: pip3 install .[ci]
       - run: python3 -m pytest test
         name: Test building EMFFAX pages
