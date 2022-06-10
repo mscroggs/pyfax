1af903deb0c8b5a81abfea36118b7db0cb7da029
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-06-10 13:00:30 +0100

    main

--- test/test_tools.py
+++ test/test_tools.py
@@ -18,7 +18,7 @@ def test_json_url_helper():
 
 def test_csv_url_helper():
     data = url_helpers.load_csv(
-        "https://raw.githubusercontent.com/mscroggs/pyfax/coverage/test/data/csv.csv")
+        "https://raw.githubusercontent.com/mscroggs/pyfax/main/test/data/csv.csv")
 
     with open(os.path.join(
         os.path.dirname(os.path.realpath(__file__)),
