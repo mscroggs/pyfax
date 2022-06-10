f50663b411871991e43019fb73795fde1726404d
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-06-10 13:00:55 +0100

    add tests, increase coverage (#7)
    * add test
    
    * update test
    
    * more test
    
    * new text
    
    * new test
    
    * add csv test
    
    * no newline
    
    * =
    
    * main

---
+++ test/test_pages.py
@@ -0,0 +1,44 @@
+from pyfax import Page, Line, Color
+import lipsum
+
+
+def test_text():
+    text = "Hello there."
+    p1 = Page(100)
+    line = Line()
+    line.start_fg(Color.DEFAULT)
+    line.add_text(text)
+    p1.set_line(2, line)
+
+    p2 = Page(100)
+    p2.add_wrapped_text(2, text)
+
+    for i, j in p1.lines.items():
+        assert str(j) == str(p2.lines[i])
+
+    assert p1.to_tti() == p2.to_tti()
+
+
+def test_block():
+    block = (
+        "xx.x.xx...x.\n"
+        "x.xx...x..x.\n"
+        "xx.xx...x.x.\n"
+    )
+    p1 = Page(100)
+    line = Line()
+    line.add_block(block, Color.RED)
+    p1.set_line(2, line)
+
+    p2 = Page(100)
+    p2.add_block(2, block, Color.RED)
+
+    for i, j in p1.lines.items():
+        assert str(j) == str(p2.lines[i])
+
+    assert p1.to_tti() == p2.to_tti()
+
+
+def text_wrapped_text():
+    p = Page(100)
+    p.add_wrapped_text(2, lipsum.generate_words(100))
--- test/test_tools.py
+++ test/test_tools.py
@@ -14,3 +14,16 @@ def test_json_url_helper():
         data2 = json.load(f)
 
     assert data == data2
+
+
+def test_csv_url_helper():
+    data = url_helpers.load_csv(
+        "https://raw.githubusercontent.com/mscroggs/pyfax/main/test/data/csv.csv")
+
+    with open(os.path.join(
+        os.path.dirname(os.path.realpath(__file__)),
+        os.path.join("data", "csv.csv"))
+    ) as f:
+        data2 = [line.split(",") for line in f]
+
+    assert data == data2
