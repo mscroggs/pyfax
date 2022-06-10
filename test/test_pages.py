5f4f9529d4cf8c174e46e41f53a8cfd56f269c2e
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-06-10 12:45:03 +0100

    update test

--- test/test_pages.py
+++ test/test_pages.py
@@ -1,13 +1,17 @@
-from pyfax import Page, Line
+from pyfax import Page, Line, Color
 
 
 def test_text():
     p1 = Page(100)
     line = Line()
+    line.start_fg(Color.DEFAULT)
     line.add_text("Hello.")
     p1.set_line(2, line)
 
-    p2 = Page(101)
+    p2 = Page(100)
     p2.add_wrapped_text(2, "Hello.")
 
-    assert p1.lines == p2.lines
+    for i, j in p1.lines.items():
+        assert str(j) == str(p2.lines[i])
+
+    assert p1.to_tti() == p2.to_tti()
