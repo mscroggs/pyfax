07245ed1ea97287c51b01bb9670e6ee35f03abb8
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-06-20 09:47:27 +0100

    skip if no file

--- test/test_tools.py
+++ test/test_tools.py
@@ -1,17 +1,21 @@
 import os
 import json
 from pyfax.tools import url_helpers
+import pytest
 
 
 def test_json_url_helper():
     data = url_helpers.load_json(
         "https://raw.githubusercontent.com/mscroggs/pyfax/main/test/data/json.json")
 
-    with open(os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        os.path.join("data", "json.json"))
-    ) as f:
-        data2 = json.load(f)
+    try:
+        with open(os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            os.path.join("data", "json.json"))
+        ) as f:
+            data2 = json.load(f)
+    except FileNotFoundError:
+        pytest.skip()
 
     assert data == data2
 
@@ -20,10 +24,13 @@ def test_csv_url_helper():
     data = url_helpers.load_csv(
         "https://raw.githubusercontent.com/mscroggs/pyfax/main/test/data/csv.csv")
 
-    with open(os.path.join(
-        os.path.dirname(os.path.realpath(__file__)),
-        os.path.join("data", "csv.csv"))
-    ) as f:
-        data2 = [line.split(",") for line in f]
+    try:
+        with open(os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            os.path.join("data", "csv.csv"))
+        ) as f:
+            data2 = [line.split(",") for line in f]
+    except FileNotFoundError:
+        pytest.skip()
 
     assert data == data2
